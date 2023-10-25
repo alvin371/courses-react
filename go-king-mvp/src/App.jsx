47d38fb88911b93cf2a22d0ed2ead11416df068f
Afitaganda <creatifypudding@gmail.com>
2023-10-25 14:32:14 +0700

    update routing 1

--- go-king-mvp/src/App.jsx
+++ go-king-mvp/src/App.jsx
@@ -2,18 +2,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-DOM'
 import './App.css'
 
+import Home from './views/Home'
+import CourseSearch from './views/courses/CourseSearch'
+import CourseDetail from './views/courses/CourseDetail'
+import LoginAdmin from './views/admin/LoginAdmin'
 import CreateCourse from './views/admin/CreateCourse'
 import ListCourse from './views/admin/ListCourse'
+import EditCourse from './views/admin/EditCourse'
+import AIChat from './views/AIchat'
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/course-search" element={<CourseSearch/>}/>
+        <Route path="/course-detail" element={<CourseDetail/>}/>
+        <Route path="/AIChat" element={<AIChat/>}/>
+
+        <Route path="/login-admin" element={<LoginAdmin/>}/>
         <Route path="/list-course" element={<ListCourse/>}/>
         <Route path="/create-course" element={<CreateCourse/>}/>
-      </Route>
+        <Route path="/edit-course" element={<EditCourse/>}/>
     </Routes>
     </BrowserRouter>
   )
